fc13006ddc49a5da1c6420d3e5799a793ad38138
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-01-21 14:40:47 -0300

    :sparkles: feat: app with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import { Home, Stack, NotFound, Portfolio } from './pages';
+import { LayoutWrapper } from './components/layout';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
-
-export function WrappedApp() {
-  return (
     <HashRouter>
-      <App />
+      <LayoutWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stack" element={<Stack />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </LayoutWrapper>
     </HashRouter>
   );
 }
+
+export default App;
