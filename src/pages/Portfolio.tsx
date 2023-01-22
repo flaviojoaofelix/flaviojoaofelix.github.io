79be9d7f5550f7f15f9237e285539d2b6a517647
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-01-22 01:51:55 -0300

    :sparkles: feat: implementing contentcard component to pages

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,31 +1,26 @@
-import portfolioItems from '../assets/data/portfolio.json';
+import { ContentCard } from '../components/layout';
 import PortfolioItem from '../components/portfolio/PortfolioItem';
+
+import portfolioItems from '../assets/data/portfolio.json';
 
 function Portfolio() {
   return (
-    <div className="card">
-      <div className="card-header">
-        <h3 className="mb-0">
-          <i className="bi bi-file-earmark-richtext-fill" /> PORTFOLIO
-        </h3>
+    <ContentCard icon="bi bi-file-earmark-richtext-fill" title="portfolio">
+      <h4 className="card-title mb-3">Meus Projetos e Trabalhos:</h4>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 g-4 justify-content-start">
+        {portfolioItems.map((item) => {
+          return (
+            <PortfolioItem
+              key={item.title}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+              url={item.url}
+            />
+          );
+        })}
       </div>
-      <div className="card-body">
-        <h4 className="card-title mb-3">Meus Projetos e Trabalhos:</h4>
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 g-4 justify-content-start">
-          {portfolioItems.map((item) => {
-            return (
-              <PortfolioItem
-                key={item.title}
-                image={item.image}
-                title={item.title}
-                description={item.description}
-                url={item.url}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
+    </ContentCard>
   );
 }
 
--- src/pages/Stack.tsx
+++ src/pages/Stack.tsx
@@ -1,34 +1,28 @@
 import { StackItem } from '../components/stack';
 
 import stack from '../assets/data/stack.json';
+import { ContentCard } from '../components/layout';
 
 function Stack() {
   return (
-    <div className="card">
-      <div className="card-header">
-        <h3>
-          <i className="bi bi-stack" /> STACK
-        </h3>
+    <ContentCard icon="bi bi-stack" title="stack">
+      <h4 className="card-title mb-3">
+        Tecnologias que mais utilizo atualmente
+      </h4>
+      <div className="row">
+        {stack.map((item) => {
+          return (
+            <StackItem
+              key={item.name}
+              url={item.url}
+              title={item.title}
+              name={item.name}
+              color={item.color}
+            />
+          );
+        })}
       </div>
-      <div className="card-body">
-        <h4 className="card-title mb-3">
-          As tecnologias que mais utilizo atualmente...
-        </h4>
-        <div className="row">
-          {stack.map((item) => {
-            return (
-              <StackItem
-                key={item.name}
-                url={item.url}
-                title={item.title}
-                name={item.name}
-                color={item.color}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
+    </ContentCard>
   );
 }
 
